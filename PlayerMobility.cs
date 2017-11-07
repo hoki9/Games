6094f58850873aa8087670e78fdff3f35f5073a9
eerotuu <32520321+eerotuu@users.noreply.github.com>
2017-11-07 17:21:48 +0200

    Add files via upload

---
+++ PlayerMobility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+
+public class PlayerMobility : MonoBehaviour
+{
+	public float speed;
+	public Rigidbody2D rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate ()
+	{
+		var mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition); //find the mouse pos.
+		Quaternion rot = Quaternion.LookRotation (transform.position - mousePosition, Vector3.forward); //find the look rotation.
+
+		transform.rotation = rot; // sets rotation of this object to be the rotation required to look at mouse.
+		transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z); // strips away x and y rotations.
+		rb.angularVelocity = 0; // prevents slide when rotating.
+
+		float input = Input.GetAxis ("Vertical");
+		rb.AddForce (gameObject.transform.up * speed * input);
+
+	}
+}
+
+
